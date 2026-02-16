c1ea7d5031daec705883fc993c3a5786c6e3dde3
Kirpichik11 <101967795+Kirpichik11@users.noreply.github.com>
2026-02-16 11:28:20 +0900

    Update public.js

--- api/public.js
+++ api/public.js
@@ -18,7 +18,7 @@ function haversineKm(a, b) {
   const dLat = toRad(b.lat - a.lat);
   const dLng = toRad(b.lng - a.lng);
   const lat1 = toRad(a.lat);
-  const lat2 = toRad(a.lat);
+  const lat2 = toRad(b.lat);
   const s =
     Math.sin(dLat / 2) ** 2 +
     Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLng / 2) ** 2;
